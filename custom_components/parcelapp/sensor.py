f6a66bb0998f895fbc4b16f5c4187b92121a57fc
George Fahmy <geofahm@gmail.com>
2025-03-12 00:42:21 -0700

    revert test

--- custom_components/parcelapp/sensor.py
+++ custom_components/parcelapp/sensor.py
@@ -26,17 +26,14 @@ async def async_setup_entry(
 ) -> None:
     """Set up the Parcel sensor platform from a config entry."""
     coordinator = hass.data[DOMAIN]["coordinator"]
-    await coordinator.async_request_refresh()
-    for delivery in coordinator.data:
-        async_add_entities([RecentShipment(coordinator, delivery)])
+    async_add_entities([RecentShipment(coordinator)])
 
 
 class RecentShipment(SensorEntity):
     """Representation of a sensor that fetches the top value from an API."""
 
-    def __init__(self, coordinator: ParcelUpdateCoordinator, delivery) -> None:
+    def __init__(self, coordinator: ParcelUpdateCoordinator) -> None:
         """Initialize the sensor."""
-        self.delivery = delivery
         self.coordinator = coordinator
         self._hass_custom_attributes = {}
         self._attr_name = "Recent Parcel Shipment"
@@ -57,41 +54,41 @@ class RecentShipment(SensorEntity):
 
     async def async_update(self) -> None:
         """Fetch the latest data from the coordinator."""
+        await self.coordinator.async_request_refresh()
+        if data := self.coordinator.data:
+            for delivery in data:
+                self._attr_name = delivery["description"]
+                if len(self._attr_name) > 20:
+                    self._attr_name = f"{self._attr_name[:20]}..."
+                try:
+                    self._attr_state = delivery["events"][0]["event"]
+                    try:
+                        event_date = delivery["events"][0]["date"]
+                    except KeyError:
+                        event_date = "Unknown"
+                    try:
+                        event_location = delivery["events"][0]["location"]
+                    except KeyError:
+                        event_location = "Unknown"
+                except KeyError:
+                    self._attr_state = "Unknown"
+                    event_date = "Unknown"
+                    event_location = "Unknown"
+                try:
+                    description = delivery["description"]
+                except KeyError:
+                    description = "Parcel"
+                try:
+                    date_expected = delivery["date_expected"]
+                except KeyError:
+                    date_expected = "Unknown"
 
-        # if data := self.coordinator.data:
-        #     for delivery in data:
-        self._attr_name = self.delivery["description"]
-        if len(self._attr_name) > 20:
-            self._attr_name = f"{self._attr_name[:20]}..."
-        try:
-            self._attr_state = self.delivery["events"][0]["event"]
-            try:
-                event_date = self.delivery["events"][0]["date"]
-            except KeyError:
-                event_date = "Unknown"
-            try:
-                event_location = self.delivery["events"][0]["location"]
-            except KeyError:
-                event_location = "Unknown"
-        except KeyError:
-            self._attr_state = "Unknown"
-            event_date = "Unknown"
-            event_location = "Unknown"
-        try:
-            description = self.delivery["description"]
-        except KeyError:
-            description = "Parcel"
-        try:
-            date_expected = self.delivery["date_expected"]
-        except KeyError:
-            date_expected = "Unknown"
-
-        self._hass_custom_attributes[description] = {
-            "latest_event": self.delivery["events"][0]["event"],
-            "tracking_number": self.delivery["tracking_number"],
-            "date_expected": date_expected.split(" ")[0],
-            "status_code": self.delivery["status_code"],
-            "carrier_code": self.delivery["carrier_code"],
-            "event_date": event_date,
-            "event_location": event_location,
-        }
+                self._hass_custom_attributes[description] = {
+                    "latest_event": delivery["events"][0]["event"],
+                    "tracking_number": delivery["tracking_number"],
+                    "date_expected": date_expected.split(" ")[0],
+                    "status_code": delivery["status_code"],
+                    "carrier_code": delivery["carrier_code"],
+                    "event_date": event_date,
+                    "event_location": event_location,
+                }
